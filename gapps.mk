e21b07f8c357567b1596fd1eb6ef7953d1bef0b3
Yumi Yukimura <me.cafebabe@gmail.com>
2024-12-24 02:02:37 +0800

    google/gsi_gms_x86_64/generic_x86_64:15/AP4A.241205.013/12621605:user/release-keys

---
+++ gapps.mk
@@ -0,0 +1,14 @@
+# From MindTheGapps
+PRODUCT_PACKAGES += \
+    GmsOverlay \
+    GmsSettingsOverlay \
+    GmsSettingsProviderOverlay \
+    GmsSetupWizardOverlay
+
+# From system image that matches with proprietary-files.txt
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.gmsversion=14_beta \
+    ro.opa.eligible_device=true \
+    ro.setupwizard.rotation_locked=true
+
+$(call inherit-product, vendor/x86_64-extras/gapps/gapps-vendor.mk)
